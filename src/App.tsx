14359a60d6de3e0af94125c2834eeca03a188055
MostafaAmrHamed <mostafaamro5619@gmail.com>
2021-11-03 09:41:30 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+type Items = {
+  TrackingNumber: string;
+  CurrentStatus: {
+    state: string;
+    timestamp: string;
+  };
+};
 
 function App() {
+  const [items, setItems] = React.useState<Items>();
+  React.useEffect(() => {
+    axios
+      .get("https://tracking.bosta.co/shipments/track/6636234")
+      .then((res) => setItems(res.data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="TEST">
+      {items ? (
+        <div>
+          <h1>{items.TrackingNumber}</h1>
+          <h1>{items.CurrentStatus.state}</h1>
+        </div>
+      ) : (
+        <h1 style={{ color: "red" }}>loading</h1>
+      )}
     </div>
   );
 }
