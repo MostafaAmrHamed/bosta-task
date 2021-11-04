9f2ac6824966cb952c059a6751e1b3fa11131e70
MostafaAmrHamed <mostafaamro5619@gmail.com>
2021-11-04 06:11:16 +0200

    feat{tracking}: designed the stats div of component

--- src/components/tracking/tracking.tsx
+++ src/components/tracking/tracking.tsx
@@ -3,5 +3,32 @@ import React from 'react';
 import styles from './tracking.module.css';
 
 export const Tracking = () => {
-  return <div className={styles.test}>Tracking order...</div>;
+  return (
+    <div className={styles.tracking}>
+      <div className={styles.container}>
+        <div className={styles.stats}>
+          <div className={styles.header}>
+            <p>header 1</p>
+            <p>header 2</p>
+            <p>header 3</p>
+            <p>header 4</p>
+          </div>
+          <div className={styles.info}>
+            <p>header 1</p>
+            <p>header 2</p>
+            <p>header 3</p>
+            <p>header 4</p>
+          </div>
+        </div>
+        <div className={styles.progress}>
+          <div>
+            <p>header 1</p>
+            <p>header 2</p>
+            <p>header 3</p>
+            <p>header 4</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
