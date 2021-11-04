6080ec2e5a2d02355923b6e0b719363bfcae10ad
MostafaAmrHamed <mostafaamro5619@gmail.com>
2021-11-04 02:02:25 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,7 @@
 import React from "react";
-import axios from "axios";
-type Items = {
-  TrackingNumber: string;
-  CurrentStatus: {
-    state: string;
-    timestamp: string;
-  };
-};
 
 function App() {
-  const [items, setItems] = React.useState<Items>();
-  React.useEffect(() => {
-    axios
-      .get("https://tracking.bosta.co/shipments/track/6636234")
-      .then((res) => setItems(res.data));
-  }, []);
-
-  return (
-    <div className="TEST">
-      {items ? (
-        <div>
-          <h1>{items.TrackingNumber}</h1>
-          <h1>{items.CurrentStatus.state}</h1>
-        </div>
-      ) : (
-        <h1 style={{ color: "red" }}>loading</h1>
-      )}
-    </div>
-  );
+  return <div>Bosta Task</div>;
 }
 
 export default App;
