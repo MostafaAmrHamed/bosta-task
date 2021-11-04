e1feecbd369063fabe005a9043f35d95307c0024
MostafaAmrHamed <mostafaamro5619@gmail.com>
2021-11-04 08:44:48 +0200

    feat{tracking}: added progress div

--- src/components/tracking/tracking.tsx
+++ src/components/tracking/tracking.tsx
@@ -7,11 +7,11 @@ export const Tracking = () => {
     <div className={styles.tracking}>
       <div className={styles.container}>
         <div className={styles.stats}>
-          <div className={styles.header}>
-            <p>header 1</p>
-            <p>header 2</p>
-            <p>header 3</p>
-            <p>header 4</p>
+          <div className={styles.header} dir="ltr">
+            <p>موعد التسليم خلال</p>
+            <p>اسم التاجر</p>
+            <p>اخر تحديث</p>
+            <p>رقم الشحنة 41654</p>
           </div>
           <div className={styles.info}>
             <p>header 1</p>
@@ -21,12 +21,10 @@ export const Tracking = () => {
           </div>
         </div>
         <div className={styles.progress}>
-          <div>
-            <p>header 1</p>
-            <p>header 2</p>
-            <p>header 3</p>
-            <p>header 4</p>
-          </div>
+          <div className={styles.track1}>T</div>
+          <div className={styles.track2}>T</div>
+          <div className={styles.track3}>T</div>
+          <div className={styles.track4}>T</div>
         </div>
       </div>
     </div>
