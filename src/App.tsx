55e30169d6b7d038e3384eea326187902f5afc0c
MostafaAmrHamed <mostafaamro5619@gmail.com>
2021-11-06 01:53:24 +0200

    feat(search-shipment): designed and fucntionality done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { Tracking, Shipment } from './components';
+import { Tracking, Shipment, TrackShipment } from './components';
 
 function App() {
   return (
     <div>
+      <TrackShipment />
       <Tracking trackingNumber={1094442} />
       <Shipment />
     </div>
